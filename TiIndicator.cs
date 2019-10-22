a9361f8d5a13ff9cd8574147d1f6d5d6e8d6f738
chinguyen1 <56132595+chinguyen1@users.noreply.github.com>
2019-10-21 18:11:26 -0700

    updated TI class

--- TiIndicator.cs
+++ TiIndicator.cs
@@ -1,25 +1,33 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace MSGraphSecurity
 {
     public class TiIndicator
     {
-        //public string Action  { get; set; }
+        //properties of TI Indicator - NEED TO FINISH THIS class
+        public string Action { get; set; }
         public List<string> ActivityGroupNames { get; set; }
+        public string AdditionalInformation { get; set; }
+        public string AzureTenantId { get; set; }
         public int Confidence { get; set; }
         public string Description { get; set; }
+        public string DiamondModel { get; set; }
         public DateTimeOffset ExpirationDateTime { get; set; }
         public string ExternalId { get; set; }
-        public string FileHashType { get; set; }
-        public string FileHashValue { get; set; }
+        public string Id { get; set; }
+        public DateTimeOffset IngestedDateTime { get; set; }
+        public bool IsActive { get; set; }
         public List<string> KillChain { get; set; }
+        public string KnownFalsePositives { get; set; }
+        public DateTimeOffset LastReportedDateTime { get; set; }
         public List<string> MalwareFamilyNames { get; set; }
+        public bool PassiveOnly { get; set; }
         public int Severity { get; set; }
         public List<string> Tags { get; set; }
         public string TargetProduct { get; set; }
         public string ThreatType { get; set; }
         public string TlpLevel { get; set; }
+        public string Url { get; set; }
     }
 }
