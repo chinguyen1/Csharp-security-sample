7a32a187bb3bd72b84d9383e3ad4919382202df4
chinguyen1 <56132595+chinguyen1@users.noreply.github.com>
2019-10-21 18:10:45 -0700

    updated  TIscontroller

--- TIIndicatorsController.cs
+++ TIIndicatorsController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -18,6 +18,7 @@ namespace MSGraphSecurity
         }
 
         #region Get, Creat, Update, Delete One TI Indicator
+        //create GET request to retrieve all TI Indicators
         public async Task<string> GetTIIndicators(string content = "")
         {
             var token = await GetToken();
@@ -46,6 +47,7 @@ namespace MSGraphSecurity
             return json;
         }
 
+        //create GET request to retrieve a single TI Indicator by Id
         public async Task<string> GetTIIndicator(string id, string content = "")
         {
             var token = await GetToken();
@@ -74,7 +76,8 @@ namespace MSGraphSecurity
             return json;
         }
 
-        public async Task<string> CreateTIIndicator(object tiIndicator)
+        //create POST request to submit a single TI Indicator
+        public async Task<string> CreateTIIndicator(TiIndicator tiIndicator)
         {
             var token = await GetToken();
 
@@ -104,7 +107,8 @@ namespace MSGraphSecurity
             return json;
         }
 
-        public async Task<string> UpdateTIIndicator(string id, object tIIndicator)
+        //create PATCH request to update a single TI Indicator by Id
+        public async Task<string> UpdateTIIndicator(string id, TiIndicator tIIndicator)
         {
             var token = await GetToken();
 
@@ -134,6 +138,7 @@ namespace MSGraphSecurity
             return json;
         }
 
+        //create DELETE request to remove a single TI Indicator by Id
         public async Task<string> DeleteTIIndicator(string id, string content = "")
         {
             var token = await GetToken();
@@ -164,74 +169,76 @@ namespace MSGraphSecurity
 
         #endregion
 
-        #region Create, Update, Delete Multiple TI Indicators - This part is currently being worked on
-        public async Task<string> CreateMultipleTIIndicators(List<TiIndicator> value)
+        #region Create, Update, Delete Multiple TI Indicators
+
+        //create POST request to submit multiple TI Indicators
+        public async Task<string> CreateMultipleTIIndicators(List<TiIndicator> indicators)
         {
-            try
+            var token = await GetToken();
+
+            string url = string.Format("https://graph.microsoft.com/beta/security/tiIndicators/submitTiIndicators");
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+
+            var obj = new { value = indicators };
+
+            var value = JsonConvert.SerializeObject(obj);
+
+            request.Content = new StringContent(value, Encoding.UTF8, "application/json");
+
+            HttpClient http = new HttpClient();
+
+            var response = await http.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
             {
-                var token = await GetToken();
-
-                //string url = string.Format("https://graph.microsoft.com/beta/security/tiIndicators/");
-                string url = string.Format("https://graph.microsoft.com/beta/security/tiIndicators/submitTiIndicators");
-
-                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, url);
-
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
-
-                var stringTIIndicator = JsonConvert.SerializeObject(value);
-
-                request.Content = new StringContent(stringTIIndicator, Encoding.UTF8, "application/json");
-
-                //request.Content = value;
-
-                HttpClient http = new HttpClient();
-
-                var response = await http.SendAsync(request);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    string error = await response.Content.ReadAsStringAsync();
-                    object formatted = JsonConvert.DeserializeObject(error);
-                    throw new WebException("Error Calling the Graph API: \n" + JsonConvert.SerializeObject(formatted, Formatting.Indented));
-                }
+                string error = await response.Content.ReadAsStringAsync();
+                object formatted = JsonConvert.DeserializeObject(error);
+                throw new WebException("Error Calling the Graph API: \n" + JsonConvert.SerializeObject(formatted, Formatting.Indented));
+            }
 
                 string json = await response.Content.ReadAsStringAsync();
                 
                 return json;
             }
-            catch (Exception ex)
+
+        public async Task<string> UpdateMultipleTIIndicators(List<TiIndicator> indicators)
+        {
+            var token = await GetToken();
+
+            string url = string.Format("https://graph.microsoft.com/beta/security/tiIndicators/updateTiIndicators");
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Patch, url);
+
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token.AccessToken);
+
+            var obj = new { value = indicators };
+
+            var value = JsonConvert.SerializeObject(obj);
+
+            request.Content = new StringContent(value, Encoding.UTF8, "application/json");
+
+            HttpClient http = new HttpClient();
+
+            var response = await http.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
             {
-                throw new Exception(ex.Message);
+                string error = await response.Content.ReadAsStringAsync();
+                object formatted = JsonConvert.DeserializeObject(error);
+                throw new WebException("Error Calling the Graph API: \n" + JsonConvert.SerializeObject(formatted, Formatting.Indented));
             }
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            return json;
         }
-
-            //var jsonList = new List<string>();
-
-            //foreach (var tiIndicator in tiIndicators)
-            //{
-            //    var stringTiIndicator = JsonConvert.SerializeObject(tiIndicator);
-
-            //    request.Content = new StringContent(stringTiIndicator, Encoding.UTF8, "application/json");
-
-            //    HttpClient http = new HttpClient();
-
-            //    var response = await http.SendAsync(request);
-
-            //    if (!response.IsSuccessStatusCode)
-            //    {
-            //        string error = await response.Content.ReadAsStringAsync();
-            //        object formatted = JsonConvert.DeserializeObject(error);
-            //        throw new WebException("Error Calling the Graph API: \n" + JsonConvert.SerializeObject(formatted, Formatting.Indented));
-            //    }
-
-            //    string json = await response.Content.ReadAsStringAsync();
-
-            //    jsonList.Add(json);
-            //}
-
 
         #endregion
 
+        //create GET request to retrieve access token
         private async Task<AuthenticationResult> GetToken()
         {
             var accessToken = new AccessToken();
